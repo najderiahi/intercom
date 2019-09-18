fe9eb222847cd97a0edc71feccb751a2194ae42b
Najd Eriahi <najdriyehi@gmail.com>
2019-09-18 10:56:08 +0100

    Initial commit #1

---
+++ database/migrations/2019_09_18_090437_create_message_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->longText('content');
+            $table->unsignedBigInteger('receiver_id')->nullable();
+            $table->unsignedBigInteger('sender_id')->nullable();
+
+            $table->foreign('sender_id')->references('id')->on('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreign('receiver_id')->references('id')->on('users')->onDelete('set null')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('message');
+    }
+}
